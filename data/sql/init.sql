8fd7a35c8e1ad334b302035a1e44d35eeae15b2a
mofmofty <moftytec@gmail.com>
2020-05-05 22:44:40 +0900

    [fix]docker修正

--- data/sql/init.sql
+++ data/sql/init.sql
@@ -1,12 +1,9 @@
-CREATE TABLE sample_table
-(
-  id              INT(10) AUTO_INCREMENT,
-  name            VARCHAR(100) NOT NULL,
-  created_at      DATETIME DEFAULT NULL,
-  modified_at     DATETIME DEFAULT NULL,
-  PRIMARY KEY (id)
-);
+#! /bin/bash
 
-INSERT INTO sample_table(name) VALUES ('name1');
-INSERT INTO sample_table(name, created_at) VALUES ('name2', '2020-02-01 12:00:00');
-INSERT INTO sample_table(name, created_at, modified_at) VALUES ('name3', '2020-03-01 12:00:00', '2020-03-01 18:00:00');
+CREATE_USER='cakephp'
+CREATE_PASSWORD='password'
+GRANT="GRANT ALL ON ${MYSQL_DATABASE}.* TO ${CREATE_USER}@'%' IDENTIFIED BY ${CREATE_PASSWORD}; FLUSH PRIVILEGES"
+
+echo $GRANT | mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD
+
+
